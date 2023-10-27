2be5b36110250e4d21ec2ec4f4db7819e540a1ee
drjyeh <52799302+drjyeh@users.noreply.github.com>
2023-10-27 15:07:11 -0700

    node js version

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
@@ -7,7 +8,8 @@ const port = 3000;
 var data = [];
 var metadata = {};
 const datafolder = './uploads/';
-const datafile = datafolder + 'sample.json';
+const jsfolder = './templates/';
+// const datafile = datafolder + 'sample.json';
 
 function getvalleys1(data, threshold, width) {
     let valleys = [];
@@ -26,6 +28,18 @@ function getvalleys1(data, threshold, width) {
     return valleys;
 }
 
+function getderiv(data) {
+    let deriv = [];
+
+    let j = data[0];
+    for (let i in data) {
+        deriv.push(i - j);
+        j = i;
+    }
+
+    return deriv;
+}
+
 function get_data(payload) {
     switch (payload['type']) {
         case 'command':
@@ -40,8 +54,21 @@ function get_data(payload) {
                 // $i = 0;
                 // while (file_exists($datafile = $datafolder.sprintf("sample%03d-%03d.json", $user, $i)))
                 //     $i += 1;
+                let user;
 
+                if (content.metadata.user == null)
+                    user = 0;
+                else 
+                    user = content.metadata.user;
 
+                let i = 0;
+                let datafile;
+                do {
+                    datafile = datafolder + "sample" + 
+                                String(user).padStart(4, '0') + "-" +
+                                String(i).padStart(4, '0') + ".json";
+                    i = i + 1;
+                } while (fs.existsSync(datafolder));
 
                 fs.writeFile(datafile, JSON.stringify(content), (err) => {
                     if (err)
@@ -72,29 +99,41 @@ function get_data(payload) {
 }
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
+    // console.log(req.url);
+    var urls = url.parse(req.url, true);
+    // console.log(urls.pathname);
+    // console.log(urls.search == null);
 
-    switch (req.url) {
+    switch (urls.pathname) {
         case '/':
-            fs.readFile('./templates/index.html', function (err, html) {
+            fs.readFile(jsfolder+'index.html', function (err, html) {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.write(html);
                 return res.end();
-            });
+            }); 
+
             break;
 
 
         case '/data':
-            fs.readdir('/var/', function(err, files){
-                files.forEach(function(file){
-                    res.write("<a href=/data?" + file + ">" + file + "</a><br>");
+            if (urls.search == null) {
+                fs.readdir(datafolder, function (err, files) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    files.forEach(function (file) {
+                        res.write("<a href=/data?" + file + ">" + file + "</a><br>");
+                    });
+                    return res.end();
                 });
-            });
-            fs.readFile(datafile, function (err, html) {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(html);
-                return res.end();
-            });
+
+            } else {
+                let datafile = datafolder  + urls.search.substring(1);
+                console.log(datafile);
+                fs.readFile(datafile, function (err, html) {
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.write(html);
+                    return res.end();
+                });
+            }
             break;
 
         case '/upload':
@@ -117,19 +156,32 @@ const server = http.createServer((req, res) => {
 
 
         case '/valleys':
-            fs.readFile(datafile, function (err, html) {
-                let dv = JSON.parse(html);
-                let len = dv.data[0].data.length;
-                let sv = Array(len).fill(0);
-                for (let i in dv.data) {
-                    sv = sv.map((n, j) => n + dv.data[i].data[j]);
-                }
-                let sm = sv.reduce((a, b) => a + b, 0) / sv.length;
-                let valleys = getvalleys1(sv, sm, 10);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.write(JSON.stringify(valleys));
-                return res.end();
-            });
+                        if (urls.search == null) {
+                fs.readdir(datafolder, function (err, files) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    files.forEach(function (file) {
+                        res.write("<a href=/valleys?" + file + ">" + file + "</a><br>");
+                    });
+                    return res.end();
+                });
+
+            } else {
+                let datafile = datafolder  + urls.search.substring(1);
+                console.log(datafile);
+                fs.readFile(datafile, function (err, html) {
+                    let dv = JSON.parse(html);
+                    let len = dv.data[0].data.length;
+                    let sv = Array(len).fill(0);
+                    for (let i in dv.data) {
+                        sv = sv.map((n, j) => n + dv.data[i].data[j]);
+                    }
+                    let sm = sv.reduce((a, b) => a + b, 0) / sv.length;
+                    let valleys = getvalleys1(sv, sm, 10);
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.write(JSON.stringify(valleys));
+                    return res.end();
+                });
+            }
             break;
 
         case '/linect':
@@ -166,6 +218,35 @@ const server = http.createServer((req, res) => {
 
         case '/linemt':
             break;
+
+        case '/plotderiv':
+        case '/plotct':
+        case '/plotgl':
+        case '/plotpl':
+            if (urls.search == null) {
+                fs.readdir(datafolder, function (err, files) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    files.forEach(function (file) {
+                        res.write("<a href="+urls.pathname+"?" + file + ">" + file + "</a><br>");
+                    });
+                    return res.end();
+                });
+
+            } else {
+                let datafile = urls.search.substring(1);
+                let jsfile = jsfolder + urls.pathname +".php";
+                console.log(datafile);
+                console.log(jsfile);
+                fs.readFile(jsfile, function (err, html) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write("<script>var filename='"+datafile+"'; </script>");
+                    res.write(html);
+                    return res.end();
+                });
+            }
+            break;
+        default:
+            res.write("NOT FOUND");
 
     }
 });
